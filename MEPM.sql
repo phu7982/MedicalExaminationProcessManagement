4436ff41cb58b58bb5a27bc05eaa04b52083a923
ntduong273 <razer3312@gmail.com>
2024-09-20 00:44:51 +0700

    adjustTable

--- MEPM.sql
+++ MEPM.sql
@@ -1,6 +1,8 @@
 ﻿CREATE DATABASE MEPM
 USE MEPM
 
+
+
 CREATE TABLE BENHNHAN 
 (
    MaBN varchar(10) primary key,
@@ -11,76 +13,99 @@ CREATE TABLE BENHNHAN
    Sdt varchar(12) unique,
    TinhTrangSK nvarchar(50)
 )
-
 SELECT * FROM benhnhan
+
+
 
 CREATE TABLE BACSI
 (
    MaBS varchar(10) primary key,
    TenBS nvarchar(50) not null,
-   ChuyenNganh nvarchar(50) not null
+   ChuyenNganh nvarchar(50) not null,
+   MaKhoa varchar(10) not null,
+   constraint RBKN_BS_KH foreign key (MaKhoa) references KHOA(MaKhoa)
 )
-
 SELECT * FROM bacsi
+
+
+
+CREATE TABLE KHOA
+(
+   MaKhoa varchar(10) primary key,
+   TenKhoa nvarchar(50) not null,
+   MaDV varchar(10) not null,
+   constraint RBKN_KH_DV foreign key (MaDV) references DICHVU(MaDV),
+)
+SELECT * FROM khoa
+
+
 
 CREATE TABLE THUOC
 (
-   MaThuoc varchar(20) primary key,
+   MaThuoc varchar(10) primary key,
    TenThuoc nvarchar(50) not null,
    GiaThuoc float check(GiaThuoc > 0)
 )
-
 SELECT * FROM thuoc
+
+
 
 CREATE TABLE DONTHUOC
 (
-   MaDT varchar(20) primary key,
-   MaBS varchar(10) references Bacsi(MaBS),     --Ràng buộc giá trị MaBS trong DONTHUOC phải nằm trong MaBS của BACSI
-   MaBN varchar(10) references Benhnhan(MaBN)   --Ràng buộc giá trị MaBN trong DONTHUOC phải nằm trong MaBN của BENHNHAN
+   MaDT varchar(10) primary key,
+   MaBS varchar(10) not null,
+   MaBN varchar(10) not null,
+   constraint RBKN_DT_BS foreign key (MaBS) references Bacsi(MaBS),     --Ràng buộc giá trị MaBS trong DONTHUOC phải nằm trong MaBS của BACSI
+   constraint RBKN_DT_BN foreign key (MaBN) references Benhnhan(MaBN)   --Ràng buộc giá trị MaBN trong DONTHUOC phải nằm trong MaBN của BENHNHAN
 )
-
 SELECT * FROM donthuoc
+
+
 
 CREATE TABLE CT_DONTHUOC 
 (
-   MaDT varchar(20) not null,
-   MaThuoc varchar(20) not null,
+   MaDT varchar(10) not null,
+   MaThuoc varchar(10) not null,
    Soluong int check(Soluong > 0)
-   constraint RBKC primary key (MaDT, MaThuoc),
-   foreign key (MaDT) references DonThuoc(MaDT),
-   foreign key (MaThuoc) references Thuoc(MaThuoc)
+   constraint RBKC_DT primary key (MaDT, MaThuoc),							--Ràng buộc khóa chính của bảng là kết hợp 2 cột MaDT và MaThuoc
+   constraint RBKN_DT_DT foreign key (MaDT) references DonThuoc(MaDT),		--Ràng buộc khóa ngoại của bảng CT_DONTHUOC với bảng DonThuoc thông qua MaDT
+   constraint RBKN_DT_Th foreign key (MaThuoc) references Thuoc(MaThuoc)	--Ràng buộc khóa ngoại của bảng CT_DONTHUOC với bảng Thuoc thông qua MaThuoc
 )
-
 SELECT * FROM ct_donthuoc
+
+
 
 CREATE TABLE DICHVU
 (
-   MaDV varchar(20) primary key,
+   MaDV varchar(10) primary key,
    TenDV nvarchar(50) not null,
    DonGia int check(DonGia > 0)
 )
-
 SELECT * FROM dichvu
+
+
 
 CREATE TABLE HOADON 
 (
-   MaHD varchar(20) primary key,
-   MaDT varchar(20) not null references DonThuoc(MaDT),
-   MaBN varchar(10) not null references BenhNhan(MaBN),
+   MaHD varchar(10) primary key,
+   MaDT varchar(10) not null, 
+   MaBN varchar(10) not null,   
+   constraint RBKN_HD_DT foreign key (MaDT) references DonThuoc(MaDT),		--Ràng buộc giá trị MaDT trong HOADON phải nằm trong MaDT của Donthuoc		
+   constraint RBKN_HD_BN foreign key (MaBN) references BenhNhan(MaBN),		--Ràng buộc giá trị MaBN trong HOADON phải nằm trong MaBN của Benhnhan
    NgayLap date not null,
    Tongtien float check(Tongtien > 0)
 )
-
 SELECT * FROM hoadon
+
+
 
 CREATE TABLE HOADONDV
 (
-   MaHD varchar(20) not null,
-   MaDV varchar(20) not null,
+   MaHD varchar(10) not null,
+   MaDV varchar(10) not null,
    Soluong int check(Soluong > 0),
-   constraint RBKC_HD primary key (MaHD, MaDV),
-   foreign key (MaHD) references Hoadon(MaHD),
-   foreign key (MaDV) references DichVu(MaDV)
+   constraint RBKC_HD primary key (MaHD, MaDV),								--Ràng buộc khóa chính của bảng là kết hợp 2 cột MaHD và MaDV
+   constraint RBKN_HD_HD foreign key (MaHD) references Hoadon(MaHD),		--Ràng buộc khóa ngoại của bảng HOADONDV với bảng Hoadon thông qua MaHD
+   constraint RBKN_HD_DV foreign key (MaDV) references DichVu(MaDV)			--Ràng buộc khóa ngoại của bảng HOADONDV với bảng Dichvu thông qua MaDV
 )
-
 SELECT * FROM hoadondv
